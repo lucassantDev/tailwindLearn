fe688be205551500d6382d1792fe58ebc8d6f8e2
Lucas Sant <lucasfdesantana095@gmail.com>
2025-04-28 22:43:20 -0300

    'feat

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,11 @@
-// const portfolio = document.getElementByClass("portfolio");
-// portfolio.addEventListener('click', ()=> {
-//     window.open = ('portfolio-lucassantdev.vercel.app')
-// })
+function portfolioDev() {
+    window.open("https://portfolio-lucassantdev.vercel.app/", "_blank");
+}
+
+function githubLucassant(){
+    window.open("https://github.com/lucassantDev", "_blank");
+}
+
+function linkedinLucassant(){
+    window.open("https://www.linkedin.com/in/lucas-ferreira-a07930309/", "_blank")
+}
